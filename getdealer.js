2e6fe05dab55873477502bb38aa8425da4ae8497
Shan Ting <shantingst@gmail.com>
2013-07-22 13:23:22 +0800

    Added

--- getdealer.js
+++ getdealer.js
@@ -1,16 +1,13 @@
-var formidable = require('formidable'),
+var fs = require('fs'),
 	async =	require('async');
-var deal={};
-deal['uploaddata']=require('./post/uploaddata').main;
-deal['submittjdaprob']=require('./post/submittjdaprob').main;
 
-function findpath(path,sql,callback){
+function findgetpath(path,sql,callback){
 	if(path.length!=33)return;
 	sql.getConnection(function(err,sqlc){
 		if(err){
 			console.log('ERROR!');
 		}else{
-			sqlc.query('SELECT * from xjos.posttable WHERE ptuuid='+sqlc.escape(path.substr(1)),
+			sqlc.query('SELECT * from xjos.gettable WHERE uuid='+sqlc.escape(path.substr(1))+' AND expire>NOW()',
 			function(err,rows){
 				if(err){
 					console.log('ERROR!');
@@ -18,52 +15,26 @@ function findpath(path,sql,callback){
 					if(rows.length<1){
 						callback('NOT FOUND');
 					}
-					callback(null,rows[0].info,rows[0].uid);
+					callback(null,rows[0].uuid,rows[0].hash);
 				}
 				sqlc.end();
 			});
 		}
-	})
+	});
 }
 exports.main=function(path,response,sql,rawreq,callback){
-	findpath(path,sql,function(err,res,uid){
+	findgetpath(path,sql,function(err,uuid,hash){
 		if(err){
 			callback(err);
 			return;
 		}
-		try{
-			var k=JSON.parse(res);
-			if(typeof(k)!='object'){
-				console.log('Post Error');
-				return;
-			}
-
-			if(deal[k.order]!=undefined){
-				var form = new formidable.IncomingForm();
-				form.parse(rawreq,function(err,fields,files){
-//					console.log(JSON.stringify(fields));
-//					console.log(JSON.stringify(files));
-					for(var i in files){
-						k.filename=files[i].name;
-						deal[k.order](files[i].path,k,uid,sql,
-						function(errv){
-							if(errv=== 'ok'){
-								response.write('ok');
-							}else{
-								response.write('err');
-							}
-						});
-					}
-					callback('ok');
-				});
-//				form.parse(rawreq);
-			}else{
-				callback('Order not defined');
-			}
-		}catch(e){
-			console.log(e);
-			callback(false);
-		}
+		var stream=fs.createReadStream('files/'+hash,{'bufferSize':64*1024}).pipe(response);
+/*		stream.on('open',function(){
+			stream.pipe(response);
+		});
+		stream.on('err',function(err){
+			res.end(err);
+		});*/
 	});
 }
 exports.check=function(path,sql,callback){
